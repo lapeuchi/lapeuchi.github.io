a20e4f72f79349f40a1200388c569cdc658e9f18
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2025-07-07 17:44:00 +0900

    검색엔진 노출 자동화
    검색엔진 노출 자동화

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import rehypeSlug from 'rehype-slug';
+import sitemap from 'astro-sitemap';
 //import content from '@astrojs/content'; // ✅ content 플러그인 추가
 // import react from '@astrojs/react'; // 필요 시 사용
 
@@ -8,6 +9,7 @@ import rehypeSlug from 'rehype-slug';
 export default defineConfig({
   outDir: 'dist',
   site: 'https://lapeuchi.github.io',
+  integrations: [sitemap()],
   base: '/',
 
   markdown: {
