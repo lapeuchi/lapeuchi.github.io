0b760a46bc4bbc21189272b016fdf8ada2065d81
lapeuchi <78643629+lapeuchi@users.noreply.github.com>
2025-07-18 15:39:58 +0900

    수식 추가
    수식 추가

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,11 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import rehypeSlug from 'rehype-slug';
 import sitemap from 'astro-sitemap';
-//import content from '@astrojs/content'; // ✅ content 플러그인 추가
-// import react from '@astrojs/react'; // 필요 시 사용
+import rehypeSlug from 'rehype-slug';
 
-// https://astro.build/config
+
 export default defineConfig({
   outDir: 'dist',
   site: 'https://lapeuchi.github.io',
@@ -19,5 +16,4 @@ export default defineConfig({
   vite: {
     assetsInclude: ['**/*.yml'],
   },
-  // integrations: [react()]
 });
